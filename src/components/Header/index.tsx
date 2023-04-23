85c42e88a5544cb1f6d5cad22e84a72a9e28899a
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2023-04-22 21:14:52 -0300

    Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="ddf-container ddf-header-content">
+        <h1>DSFilter</h1>
+        <div className="ddf-header-products-quantity">6 produto(s)</div>
+      </div>
+    </header>
+  );
+}
